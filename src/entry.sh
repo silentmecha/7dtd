1cddff0b806548435f32c671a7cbc3f082d02806
root <root@localhost.localdomain>
2021-04-23 15:22:16 +0200

    Add in configuration change sample

--- src/entry.sh
+++ src/entry.sh
@@ -4,6 +4,8 @@
 if [ ! -f "${STEAM_SAVEDIR}/serverconfig.xml" ]; then
     cp "${STEAMAPPDIR}/serverconfig.xml" ${STEAM_SAVEDIR}
     # Add in config file modification using environment variables
+    # Example:
+    # xmlstarlet ed --inplace -u '/ServerSettings/property[@name="ServerName"]/@value' -x 'string("I have changed")' serverconfig.xml
 fi
 
 # Add in warning for online players using telnet much like for PixARK
